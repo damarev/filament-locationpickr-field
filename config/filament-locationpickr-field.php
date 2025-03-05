7263eadfa1b96a89652f94ad92f23b3c55309c15
Dav Mat <damarev@gmail.com>
2025-03-05 18:08:18 +0100

    Fix v1.0.1

--- config/filament-locationpickr-field.php
+++ config/filament-locationpickr-field.php
@@ -7,7 +7,7 @@ return [
     | Default Plugin Options
     |--------------------------------------------------------------------------
     */
-    'key' => env('GMAP_API', ''),
+    'key' => env('GOOGLE_MAPS_API_KEY', ''),
 
     'default_location' => [
         'lat' => 41.32836109345274,
